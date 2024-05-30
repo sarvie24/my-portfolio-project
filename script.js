fd3cff6915a07b3c2afb38c77f9cc050e6ccd626
sarvie24 <samuelnnah080@gmail.com>
2024-05-30 02:09:34 +0100

    shop pages update

--- script.js
+++ script.js
@@ -12,4 +12,21 @@ if (close){
     close.addEventListener('click', () => {
         nav.classList.remove('active');
     })
+
+
+var MainImg = document.getElementById("MainImg")
+var smallImg = document.getElementsByClassName("small-img")
+
+    smallImg[0].onclick = function() {
+        MainImg.src = smallImg[0].src;
+    }
+    smallImg[1].onclick = function() {
+        MainImg.src = smallImg[1].src;
+    }
+    smallImg[2].onclick = function() {
+        MainImg.src = smallImg[2].src;
+    }
+    smallImg[3].onclick = function() {
+        MainImg.src = smallImg[3].src;
+    }
 }
